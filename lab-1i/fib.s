8d5d0ccdf0fced3d92c2d625e773b2608cc6a6b1
ItsDin <imdindini88@gmail.com>
2015-10-01 23:43:24 +0800

    This is Test git commit at 23:44
    It contains my homeworks and practice.

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -11,33 +11,29 @@
 fibonacci:
 	@ ADD/MODIFY CODE BELOW
 	@ PROLOG
-	push {r3, r4, r5, lr}
+	push {r3, r4, r5, r6, lr}
 
+	subs r4,r0,#0
+	mov r3, #-1
 	@ R4 = R0 - 0 (update flags)
+	mov r4, #1
 	@ if(R0 <= 0) goto .L3 (which returns 0)
+	mov r6, #0
+	mov r5, #0
+.L3:
+	add r5,r4,r3
+	mov r3,r4
+	mov r4,r5
 
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
-
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
-
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
-
-	@ R0 = R5 + R0 (update flags)
-
-	pop {r3, r4, r5, pc}		@EPILOG
+	
+	add r6,r6, #1
+	cmp r0, r6
+	bcs .L3
+	
+	mov r0,r4
+	pop {r3, r4, r5, r6, pc}		@EPILOG
 
 	@ END CODE MODIFICATION
-.L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
-
-.L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
 
 	.size fibonacci, .-fibonacci
 	.end
