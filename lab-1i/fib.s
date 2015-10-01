9233f2b2206836861f98e8255525dca90c15d413
ItsDin <imdindini88@gmail.com>
2015-10-02 04:33:14 +0800

    This is the Iterative Fibonacci(push r6 register).
    I push the r6 register to save i of 'for loop'.
    Initialize value to 'pre=-1'.'result=1' and 'sum=0'.
    Give value to the fist two of current value and use the 'for loop'
    to finish.

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -11,33 +11,39 @@
 fibonacci:
 	@ ADD/MODIFY CODE BELOW
 	@ PROLOG
-	push {r3, r4, r5, lr}
+	push {r3, r4, r5, r6, lr}
 
+	subs r4,r0,#0
 	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
+	mov r3, #-1
+	@ R3 is pre = -1
+	mov r4,#1
+	@ R4 is result = 1
+	mov r6,#0
+	@ R6 is i
+	mov r5,#0
+	@ R5 is sum = 0
+.L3:
+	add r5,r4,r3
+	@ Sum = Result + Pre
+	mov r3,r4
+	@ Pre = Result
+	mov r4,r5
+	@Result = Sum
+	
 
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
+	add r6,r6,#1
+	@ R6 = R6+1
+	cmp r0,r6
+	@ Compare R0 with R6
+	bcs .L3
+	@ If R0 >= R6 goto loop .L3
 
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
-
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
-
-	@ R0 = R5 + R0 (update flags)
-
-	pop {r3, r4, r5, pc}		@EPILOG
+	mov r0,r4
+	pop {r3, r4, r5, r6 , pc}		@EPILOG
 
 	@ END CODE MODIFICATION
-.L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
 
-.L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
 
 	.size fibonacci, .-fibonacci
 	.end
